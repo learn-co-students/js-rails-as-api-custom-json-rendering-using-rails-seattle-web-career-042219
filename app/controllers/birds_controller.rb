698cb2a2631c4735d757b3ed78918d4d3bb6cefe
Sean Watters <sean@watters.io>
2019-06-13 11:44:30 -0700

    Done.

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,15 @@
 class BirdsController < ApplicationController
   def index
     @birds = Bird.all
-    render json: @birds
+    render json: birds, only: [:id, :name, :species]
+    # render json: birds, except: [:created_at, :updated_at]
+  end
+
+  def show
+    if bird
+      render json: { id: bird.id, name: bird.name, species: bird.species }
+    else
+      render json: { message: 'Bird not found' }
+    end
   end
 end
